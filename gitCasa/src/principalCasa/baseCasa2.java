6157377d4682bb0e0d01de924ee7ff053c43d908
Gorka <Gorka@DESKTOP-F9U00JN>
2024-11-20 23:18:56 +0100

    commit numero 2 casa

--- gitCasa/src/principalCasa/baseCasa2.java
+++ gitCasa/src/principalCasa/baseCasa2.java
@@ -1,11 +1,10 @@
 package principalCasa;
 
-public class baseCasa {
+public class baseCasa2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hola");
-		System.out.println("adios");
+		System.out.println("buenas noches");
 	}
 
 }
